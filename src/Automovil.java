9c1ac521bb0b0f709d94d51fa1dc6161c4f30d2d
Arsereg <contacto@ginomarin.com>
2016-09-22 02:07:41 -0600

    Merge origin/master

--- src/Automovil.java
+++ src/Automovil.java
@@ -1,13 +1,298 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author arser
- */
 public class Automovil {
+
+    private String modelo;
+    private String placa;
+    private String serie;
+    private String marca;
+    private boolean encendido;
+    private boolean abierto;
+    private boolean arrancado;
+    private String contrasenia;
+    private int velocidad;
+    public static int MAX_VELOCIDAD = 120;
+    static int enumeradorCarro = 1;
+    
+    /**
+     * @return the MAX_VELOCIDAD
+     */
+    public static int getMAX_VELOCIDAD() {
+        return MAX_VELOCIDAD;
+    }
+    
+    public Automovil(){
+        modelo = "";
+        placa = "";
+        setSerie();
+        marca = "";
+        encendido = false;
+        abierto = false;
+        arrancado = false;
+        velocidad = 0;
+        contrasenia = "123";
+    }
+    
+    public Automovil(String pmodelo, String pplaca, String pmarca){
+        modelo = pmodelo;
+        placa = pplaca;
+        setSerie();
+        marca = pmarca;
+        encendido = false;
+        abierto = false;
+        arrancado = false;
+        velocidad = 0;
+        contrasenia = "123";
+    }
+    
+    public Automovil(String pplaca){
+        modelo = "";
+        placa = pplaca;
+        setSerie();
+        marca = "";
+        encendido = false;
+        abierto = false;
+        arrancado = false;
+        velocidad = 0;
+        contrasenia = "123";
+    }
+
+    /**
+     * @return the modelo
+     */
+    public String getModelo() {
+        return modelo;
+    }
+
+    /**
+     * @param modelo the modelo to set
+     */
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    /**
+     * @return the placa
+     */
+    public String getPlaca() {
+        return placa;
+    }
+
+    /**
+     * @param placa the placa to set
+     */
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    /**
+     * @return the serie
+     */
+    public String getSerie() {
+        return serie;
+    }
+
+    /**
+     * @param serie the serie to set
+     */
+    private void setSerie() {
+        this.serie = generarSerial();
+    }
+
+    /**
+     * @return the marca
+     */
+    public String getMarca() {
+        return marca;
+    }
+
+    /**
+     * @param marca the marca to set
+     */
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    /**
+     * @return the encendido
+     */
+    public boolean isEncendido() {
+        return encendido;
+    }
+
+    /**
+     * @param encendido the encendido to set
+     */
+    public void setEncendido(boolean encendido) {
+        this.encendido = encendido;
+    }
+
+    /**
+     * @return the abierto
+     */
+    public boolean isAbierto() {
+        return abierto;
+    }
+
+    /**
+     * @param abierto the abierto to set
+     */
+    public void setAbierto(boolean abierto) {
+        this.abierto = abierto;
+    }
+
+    /**
+     * @return the arrancado
+     */
+    public boolean isArrancado() {
+        return arrancado;
+    }
+
+    /**
+     * @param arrancado the arrancado to set
+     */
+    public void setArrancado(boolean arrancado) {
+        this.arrancado = arrancado;
+    }
+
+    /**
+     * @return the velocidad
+     */
+    public int getVelocidad() {
+        return velocidad;
+    }
+
+    /**
+     * @param velocidad the velocidad to set
+     */
+    public void setVelocidad(int velocidad) {
+        this.velocidad = velocidad;
+        if(velocidad <= 0){
+            setArrancado(false);
+        }
+    }
+
+    /**
+     * @param pass the pass to set
+     */
+    public void setPass(String ppass) {
+        this.contrasenia = ppass;
+    }
+    
+    static int getEnumerador(){
+        int resul = enumeradorCarro;
+        return resul;
+    }
+    
+    private String generarSerial(){
+        Automovil serializador = null;
+        int numSerie = serializador.getEnumerador();
+        String resul = "SC-" + numSerie;
+        enumeradorCarro++;
+        return resul;
+    }
+    
+    private int getVelMax(){
+        int resul = MAX_VELOCIDAD;
+        return resul;
+    }
+    
+    public String toString(){
+        String model = "Modelo: " + modelo;
+        String plate = "Placa: " + placa;
+        String serialNum = "Serie: " +serie;
+        String brand = "Marca:" + marca;
+        String on = "Encendido: " + encendido;
+        String open = "Abierto: " + abierto;
+        String driving = "Arrancado:" + arrancado;
+        String pass = "Contrasenia: " + contrasenia;
+        String speed = "Velocidad: " + velocidad;
+        String resul = model + "\n" + plate + "\n" + serialNum + "\n" + brand + "\n" + on + "\n" + open + "\n" + driving + "\n" + pass + "\n" + speed;
+        return resul;
+    }
+    
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /*Comienza lógica de negocio*/
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    public void encender(){
+        if(isAbierto()){
+            if(!isEncendido()){
+                setEncendido(true);
+            }
+        }
+    }
+    
+    public void apagar(){
+        if(isAbierto()){
+            if(isEncendido()){
+                detener();
+                setEncendido(false);
+            }
+        }
+    }
+    
+    public void arrancar(){
+        if(isAbierto()){
+            if(isEncendido()){
+                if(!isArrancado()){
+                    setArrancado(true);
+                    acelerar();
+                }
+            }
+        }
+    }
+    
+    public void acelerar(){
+        if(isAbierto()){
+            if(isArrancado()){
+                int incremento = 5;
+                int velActual = getVelocidad();
+                int nuevaVel = velActual + incremento;
+                int maxVel = this.getVelMax();
+                if(nuevaVel > maxVel){
+                    nuevaVel = maxVel;
+                }
+                if(nuevaVel > velActual){
+                    setVelocidad(nuevaVel);
+                }
+            }
+        }
+    }
+    
+    public void frenar(){
+        if(isAbierto()){
+            int velActual = getVelocidad();
+            int indiceFrenado = 5;
+            int nuevaVel = 0;
+            if(velActual >= indiceFrenado){
+                nuevaVel = velActual - indiceFrenado;
+            }
+            if(nuevaVel < 0){
+                nuevaVel = 0;
+            }
+            setVelocidad(nuevaVel);
+        }
+    }
+    
+    public void detener(){
+        if(isAbierto()){
+            setVelocidad(0);
+        }
+    }
+    
+    public boolean abrir(String ppass){
+        boolean seAbrio = ppass.equals(contrasenia);
+        if(seAbrio){
+            setAbierto(true);
+        }
+        return seAbrio;
+    }
+    
+    public void cerrar(){
+        if(isAbierto()){
+            apagar();
+            setAbierto(false);
+        }
+    }
     
 }
--- src/IUAutomovil.java
+++ src/IUAutomovil.java
@@ -1,3 +1,153 @@
+
+import java.io.*;
+
 public class IUAutomovil {
+    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    static PrintStream out = System.out;
+    static Automovil[] autos = new Automovil[1];
+    static int autoSelect = 0;
+    public static void main(String[] args)throws java.io.IOException{
+        autos[0] = new Automovil();
+        int accion = -1;
+        do{
+            mostrarMenu();
+            accion = Integer.parseInt(in.readLine());
+            if(accion != 0){
+                realizarAccion(accion);
+            }
+        }while(accion != 0);
+    }
     
+    static void mostrarMenu(){
+        String[] listaMenu = {
+            "Digite 1 para Abrir",
+            "Digite 2 para Arrancar",
+            "Digite 3 para Acelerar",
+            "Digite 4 para Frenar",
+            "Digite 5 para Detenerse",
+            "Digite 6 para Cerrar",
+            "Digite 7 para ver el estado del carro",
+            "Digite 0 para salir"
+        };
+        
+        for(int i = 0; i < listaMenu.length; i++){
+            out.println(listaMenu[i]);
+        }
+    }
+    
+    static void realizarAccion(int paccion)throws java.io.IOException{
+        if(paccion == 0 || paccion == 1 || paccion == 6 || paccion == 7){
+            switch(paccion){
+                case 0:
+                    desplegarDespedida();
+                    break;
+                case 1:
+                    intentarAbrir();
+                    break;
+                case 6:
+                    cerrar();
+                    break;
+                case 7:
+                    verEstado();
+                    break;
+            }
+        }else{
+            if(paccion > 0 && paccion <= 5){
+                if(autos[autoSelect].isAbierto()){
+                    switch(paccion){
+                    case 1:
+                        intentarAbrir();
+                        break;
+                    case 2:
+                        intentarArrancar();
+                        break;
+                    case 3:
+                        intentarAcelerar();
+                        break;
+                    case 4:
+                        intentarFrenar();
+                        break;
+                    case 5:
+                        detenerse();
+                        break;
+                    }
+                }else{
+                    out.println();
+                    out.println("El automovil debe estar abierto para realizar esa accion");
+                    out.println();
+                }
+            }else{
+                desplegarErrorInvalido();
+            }
+        }
+    }
+    
+    static void intentarAbrir()throws java.io.IOException{
+        out.println("");
+        out.print("Digite la contrasenia para abrir el carro: ");
+        String pass = in.readLine();
+        boolean seAbrio = autos[autoSelect].abrir(pass);
+        if(seAbrio){
+            out.println();
+            out.println("El automovil se ha abierto. Puede comenzar a conducir");
+            out.println();
+        }else{
+            out.println();
+            out.println("Contrasenia invalida. Intentelo de nuevo");
+            out.println();
+        }
+    }
+    
+    static void intentarArrancar(){
+        autos[autoSelect].arrancar();
+        out.println();
+        out.println("Automovil arrancado");
+        out.println();
+    }
+    
+    static void intentarAcelerar(){
+        autos[autoSelect].acelerar();
+        out.println();
+        out.println("Acelerando");
+        out.println();
+    }
+    
+    static void intentarFrenar(){
+        autos[autoSelect].frenar();
+        out.println();
+        out.println("Frenando");
+        out.println();
+    }
+    
+    static void detenerse(){
+        autos[autoSelect].detener();
+        out.println();
+        out.println("Auto Detenido");
+        out.println();
+    }
+    
+    static void cerrar(){
+        autos[autoSelect].cerrar();
+        out.println();
+        out.println("El auto se ha detenido.");
+        out.println("El auto se ha apagado");
+        out.println("El auto se ha cerrado");
+        out.println();
+    }
+    
+    static void verEstado(){
+        out.println();
+        out.println(autos[autoSelect]);
+        out.println();
+    }
+    
+    static void desplegarDespedida(){
+        out.println();
+        out.println("Gracias por usar nuestro servicio. Hasta pronto");
+    }
+    
+    static void desplegarErrorInvalido(){
+        out.println("Esa no es una opcion valida. Por favor intentelo de nuevo");
+        out.println();
+    }
 }
